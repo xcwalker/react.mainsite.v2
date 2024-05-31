1e8e37a5ddc88c8d82da37ef49a73aa063c00eb6
Xander Walker <xander.walker@xcw.one>
2024-05-31 14:03:21 +0100

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useCallback, useState } from "react";
+import { Fragment } from "react";
 import Logo from "./Logo";
 import { Link } from "react-router-dom";
 
@@ -9,39 +9,16 @@ const navItems = [
   {
     title: "Test",
     href: "test",
+    type: "normal",
+  },
+  {
+    title: "Contact",
+    href: "ticket",
+    type: "primary",
   },
 ];
 
 export default function Header() {
-  const [navScrollLastKnown, setNavScrollLastKnown] = useState(0);
-
-  const navScroll = useCallback(() => {
-    if (navScrollLastKnown === window.scrollY) return;
-
-    if (window.scrollY > navScrollLastKnown) {
-      document.body.classList.remove("scrolledUp");
-    } else if (window.scrollY < navScrollLastKnown - 25) {
-      document.body.classList.add("scrolledUp");
-    }
-
-    setNavScrollLastKnown(window.scrollY);
-
-    if (document.body.getBoundingClientRect().top >= 0) {
-      document.body.classList.remove("scrolled");
-    } else if (document.body.getBoundingClientRect().top < 0) {
-      // console.log(document.body.getBoundingClientRect().top)
-      document.body.classList.add("scrolled");
-    }
-  }, [navScrollLastKnown]);
-
-  useEffect(() => {
-    document.addEventListener("scroll", navScroll);
-
-    return () => {
-      document.removeEventListener("scroll", navScroll);
-    };
-  }, [navScroll]);
-
   return (
     <header className={css.header}>
       <div className={css.container}>
@@ -53,11 +30,11 @@ export default function Header() {
             navItems.map((item, index) => {
               return (
                 <Fragment key={index}>
-                  <NavLink {...item} />
+                  {item.type === "normal" && <NavLink {...item} />}
+                  {item.type === "primary" && <PrimaryButtonLink href={item.href}>{item.title}</PrimaryButtonLink>}
                 </Fragment>
               );
             })}
-          <PrimaryButtonLink href="/login">Login</PrimaryButtonLink>
         </ul>
       </div>
     </header>
@@ -71,3 +48,34 @@ function NavLink(props: { href: string; title: string }) {
     </li>
   );
 }
+
+// function Unused() {
+//   const [navScrollLastKnown, setNavScrollLastKnown] = useState(0);
+//
+//   const navScroll = useCallback(() => {
+//     if (navScrollLastKnown === window.scrollY) return;
+//
+//     if (window.scrollY > navScrollLastKnown) {
+//       document.body.classList.remove("scrolledUp");
+//     } else if (window.scrollY < navScrollLastKnown - 25) {
+//       document.body.classList.add("scrolledUp");
+//     }
+//
+//     setNavScrollLastKnown(window.scrollY);
+//
+//     if (document.body.getBoundingClientRect().top >= 0) {
+//       document.body.classList.remove("scrolled");
+//     } else if (document.body.getBoundingClientRect().top < 0) {
+//       // console.log(document.body.getBoundingClientRect().top)
+//       document.body.classList.add("scrolled");
+//     }
+//   }, [navScrollLastKnown]);
+//
+//   useEffect(() => {
+//     document.addEventListener("scroll", navScroll);
+//
+//     return () => {
+//       document.removeEventListener("scroll", navScroll);
+//     };
+//   }, [navScroll]);
+// }
