42967ab9565e6a78519e241446e93f80a35f9afa
Xander Walker <xander@xcw.one>
2024-06-10 15:18:03 +0100

    Hero Changes.

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -6,6 +6,8 @@ export default function Hero() {
   return (
     <>
       <Section id="hero" className={css.hero} container={{ className: css.container }}>
+            <img src="/background.svg" alt="" className={css.background} />
+            <div className={css.backdrop}></div>
             <Logo type="xcwalker" className={css.logo} />
       </Section>
     </>
